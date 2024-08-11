57eb5dd0f845ce240fcc8023cf086f3b15a2d3d2
Mark McGuire <mark.mcguire@Tests-MacBook-Pro.local>
2024-08-10 22:00:24 -0700

    updated single article page

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -10,10 +10,18 @@ export default async function Article({ params }: Params) {
     }
 
     return (
-        <div>
-            <h1>{article.title}</h1>
-            <small>{article.date}</small>
-            <div dangerouslySetInnerHTML={{ __html: article.content}} />
+        <div className="flex flex-col px-[calc(12vw)] py-[calc(4vh)]">
+            <div className="flex flex-col gap-2 pb-12">
+                <h1 className="text-6xl font-bold opacity-75 color-brand-dark">{article.title}</h1>
+                <h1 className="text-4xl font-semibold opacity-50 color-brand-dark">{article.subtitle}</h1>
+            </div>
+            <div className="flex">
+                <div className="bg-slate-200 h-[5px] w-4/5 rounded"></div>
+            </div>
+            <div className="flex gap-6 items-center p-2">
+                <h1 className="text-l opacity-50 color-brand-dark">{article.date}</h1>
+                <h1 className="text-l bg-[#e6d1d6] opacity-75 color-brand-dark border-2 rounded-full px-3 py-1">History</h1>
+            </div>
         </div>
     )
 }
