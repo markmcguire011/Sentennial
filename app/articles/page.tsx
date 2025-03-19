b0d946b90a95b867026af26b59fc9feb423c34c7
Mark McGuire <mcguirenumber1@gmail.com>
2025-03-18 22:16:30 -0700

    more versioning changes

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -2,13 +2,11 @@ import Articles from "@/components/content/articles";
 import { Article } from "@/interfaces/article";
 import { getAll } from "@/lib/api";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const params = await searchParams;
-  const currentPage = Number(params.page) || 1;
+type Params = Promise<{ page: string }>;
+
+export default async function Page({ params }: { params: Params }) {
+  const { page } = await params;
+  const currentPage = Number(page) || 1;
   const articles = getAll("articles");
 
   return <Articles articles={articles as Article[]} page={currentPage} />;
--- app/musings/page.tsx
+++ app/musings/page.tsx
@@ -2,13 +2,11 @@ import Musings from "@/components/content/musings";
 import { Musing } from "@/interfaces/musing";
 import { getAll } from "@/lib/api";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const params = await searchParams;
-  const currentPage = Number(params.page) || 1;
+type Params = Promise<{ page: string }>;
+
+export default async function Page({ params }: { params: Params }) {
+  const { page } = await params;
+  const currentPage = Number(page) || 1;
   const musings = getAll("musings");
 
   return <Musings musings={musings as Musing[]} page={currentPage} />;
