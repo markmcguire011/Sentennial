50f733fda4a0bf1b44e02cdb14abf01aa70199d9
Mark McGuire <mcguirenumber1@gmail.com>
2025-04-03 16:36:02 -0700

    color changes

--- components/content/articles.tsx
+++ components/content/articles.tsx
@@ -238,11 +238,11 @@ function FilterButton({
     <button
       onClick={onClick}
       className={clsx(
-        "relative py-2 px-4 rounded-full font-medium transition-all duration-200 text-md",
+        "relative py-2 px-4 rounded-full font-normal transition-all duration-200 text-md",
         "transform active:scale-95",
         {
           // not selected
-          "bg-white text-brand-dark opacity-85": !isSelected,
+          "bg-white text-brand-dark opacity-75": !isSelected,
           [colors.hover]: !isSelected,
           "shadow-md": !isSelected,
 
--- components/content/category.tsx
+++ components/content/category.tsx
@@ -20,7 +20,7 @@ export default function Category({ name }: { name: string }) {
   return (
     <h1
       style={{ backgroundColor: bg }}
-      className={`text-l opacity-75 rounded-full px-3 py-1`}
+      className={`text-l text-black opacity-40 font-normal rounded-full px-3 py-1`}
     >
       {name}
     </h1>
